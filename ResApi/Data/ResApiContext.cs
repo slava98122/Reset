195aa2c9d361004e5b629f598bfd81054237299e
simon trani <simont700@outlook.com>
2020-04-18 14:50:58 +0700

    q

---
+++ ResApi/Data/ResApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ResApi;
+
+namespace ResApi.Data
+{
+    public class ResApiContext : DbContext
+    {
+        public ResApiContext (DbContextOptions<ResApiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ResApi.User> User { get; set; }
+    }
+}
---
+++ ResApi/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ResApi
+{
+    public class User
+    {
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+
+        }
+        private string _login;
+
+        public string Login
+        {
+            get { return _login; }
+            set { _login = value;}
+       
+       
+        }
+        private string _password;
+
+        public string Password
+        {
+            get { return _password; }
+            set { _password = value; }
+
+        }
+
+    }
+}
